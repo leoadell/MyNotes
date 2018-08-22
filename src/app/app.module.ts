14d532a1ef7cb8e2472415d44e20a687840042fe
Leonardo Adell <leoadell@gmail.com>
2018-08-21 22:23:36 -0300

    my notes commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import{AngularFireAuthModule} from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { AppRouters } from './app.routes';
+import { DataService } from './data/data.service';
+
+
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRouters,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyA4VNii-cu5TZ7q9b1QOgkd8avf2TsGoAM",
+      authDomain: "mynotes-ffc84.firebaseapp.com",
+      databaseURL: "https://mynotes-ffc84.firebaseio.com",
+      projectId: "mynotes-ffc84",
+      storageBucket: "mynotes-ffc84.appspot.com",
+      messagingSenderId: "804610199676"
+    }),
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [
+    DataService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatButtonModule,
+  MatTableModule,
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
+  declarations: []
+})
+export class MaterialModule { }
