cda1216b210fcd926c28a113cb49078bef70d959
Leonardo Adell <leoadell@gmail.com>
2018-09-09 17:26:28 -0300

    auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { AuthService } from './authentication/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  email: string;
+
+  constructor(
+    private _fireAuth: AngularFireAuth,
+    private _router: Router,
+    private _authService : AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this._fireAuth.user.subscribe(user => {
+      this.email = user.email;
+    })
+  }
+
+
+  logout() {
+    this._authService.logout();
+  }
 }
--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,12 +1,67 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
-  isAuthenticated=()=>{
-    return JSON.parse(sessionStorage.getItem('isLoggedIn'));
+  constructor(
+    private _router: Router,
+    private _fireAuth: AngularFireAuth
+
+  ) { }
+
+  public isAuthenticated(): boolean {
+    // Check whether the current time is past the
+    // Access Token's expiry time
+
+    // return JSON.parse(sessionStorage.getItem('isLoggedIn'));
+
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
   }
+
+  /*
+    public handleAuthentication(): void {
+      this._fireAuth.
+      this.auth0.parseHash((err, authResult) => {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          window.location.hash = '';
+          this.setSession(authResult);
+          this.router.navigate(['/dashboard']);
+        } else if (err) {
+          this.router.navigate(['/']);
+          console.log(err);
+        }
+      });
+    }
+  */
+  private setSession(authResult): void {
+    // Set the time that the Access Token will expire at
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  public logout(): void {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // Go back to the home route
+    this._router.navigate(['/']);
+    /**
+    this._fireAuth.auth.signOut()
+      .then(() => {
+        sessionStorage.clear();
+        this._router.navigate(['/login']);
+      }); */
+  }
+
+
+
+
 }
